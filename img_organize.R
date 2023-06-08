52b36cc1bd44d9c9e8460b40944b6dc992f1300d
kroppheather <kroppheather@gmail.com>
2023-06-08 15:59:55 -0400

    adding images

--- img_organize.R
+++ img_organize.R
@@ -10,3 +10,19 @@ library(dplyr)
 
 
 imgDir <- "/media/hkropp/research/Kolyma_Data/georef_img"
+
+
+imgList <- c("A006_c.tif","a006_d.tif",
+             "A007_ct3.tif","A007_d.tif",
+             "A008_c.tif","A008_d.tif",
+             "A009_c.tif","A009_d.tif",
+             "A010_b.tif", "A010_c.tif","A010_d.tif")
+
+imgR <- list()
+for(i in 1:length(imgList)){
+  imgR[[i]] <- rast(paste0(imgDir,"/", imgList[i]))
+  
+}
+
+plot(imgR[[1]], col=grey(1:100/100))
+imgR[[1]]@ptr$res
