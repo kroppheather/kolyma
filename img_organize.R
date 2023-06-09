412004c143ba98b3708d7fa8f59b409ff31ccc82
kroppheather <kroppheather@gmail.com>
2023-06-09 12:29:23 -0400

    working on reprojecting and cropping

--- img_organize.R
+++ img_organize.R
@@ -10,7 +10,7 @@ library(dplyr)
 
 
 imgDir <- "/media/hkropp/research/Kolyma_Data/georef_img"
-extDir <- "/media/hkropp/research/Kolyma_Data/polygon_laea"
+extDir <- "/media/hkropp/research/Kolyma_Data/extent_LAEA"
 
 imgList <- c("A006_c.tif","a006_d.tif",
              "A007_ct3.tif","A007_d.tif",
@@ -18,14 +18,35 @@ imgList <- c("A006_c.tif","a006_d.tif",
              "A009_c.tif","A009_d.tif",
              "A010_b.tif", "A010_c.tif","A010_d.tif")
 
+extList <- c("A006_c.shp","a006_d.shp",
+             "A007_c.shp","A007_d.shp",
+             "A008_c.shp","A008_d.shp",
+             "A009_c.shp","A009_d.shp",
+             "A010_b.shp", "A010_c.shp","A010_d.shp")
+# read in raster images.
 imgR <- list()
 for(i in 1:length(imgList)){
   imgR[[i]] <- rast(paste0(imgDir,"/", imgList[i]))
   
 }
-
 plot(imgR[[1]], col=grey(1:100/100))
-imgR[[1]]@ptr$res
 
+# read in polygons that give the extent of usuable space in an image
+
+imgR[[1]]@ptr$res
+for(i in 1:length(extList))
 polyC <- vect(paste0(extDir, "/A006_c.shp"))
 crs(polyC)
+
+imgRrp <- project(imgR[[1]], crs(polyC), method="bilinear")
+plot(imgRrp, col=grey(1:100/100))
+imgRrp@ptr$res
+imgR[[1]]@ptr$res
+crs(imgR[[1]])
+ext(imgRrp)
+imgTest1 <- crop(imgRrp, ext(24600,24950, -2345600,-2345400))
+plot(imgTest1, col=grey(1:100/100), maxcell=ncell(imgTest1))
+imgTest2 <- crop(imgR[[1]], ext(17989600,17990300,10710500,10711000))
+plot(imgTest2, col=grey(1:100/100), maxcell=ncell(imgTest2))
+
+
