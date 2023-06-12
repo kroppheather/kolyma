e9a5f01411a72fb7a29c7fde880aa44920dff2bc
Heather Kropp <kroppheather@gmail.com>
2023-06-12 14:10:23 -0400

    Update orig_image_crop.R

--- orig_image_crop.R
+++ orig_image_crop.R
@@ -18,14 +18,21 @@ imgC <- list()
 
 for(i in 1:length(files)){
   imgC[[i]] <- crop(img[[i]], ext(.25, 25.75, 1.1, 15.9))
+  crs(imgC[[i]]) <- NA
 }
 
-par(mfrow=c(2,1))
-plot(imgC[[4]], col=gray(1:100/100))
-plot(imgC[[2]], col=gray(1:100/100))
 
 
-plot(imgC[[6]], col=gray(1:100/100))
-plot(imgC[[4]], col=gray(1:100/100))
+for(i in 1:length(files)){
+  writeRaster(imgC[[i]], paste0("e:/Google Drive/research/projects/Kolyma/georef_files/",files[i]))
+}
 
-par(mfrow=c(2,1))
+plot(imgC[[1]], col=gray(1:100/100))
+str(img[[1]]@ptr)
+help("writeRaster")
+
+crs(imgC[[1]]) <- NA
+
+writeRaster(imgC[[1]], paste0("e:/Google Drive/research/projects/Kolyma/georef_files/",files[1]))
+
+crs(imgC[[2]])
