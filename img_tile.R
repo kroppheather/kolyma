2f6ace2fed3d51909cb2fcce490b8e45abc8eabe
kroppheather <kroppheather@gmail.com>
2023-06-15 09:40:11 -0400

    working on masks

--- img_tile.R
+++ img_tile.R
@@ -17,15 +17,18 @@ install.packages("stars")
 imgDir <- "/media/hkropp/research/Kolyma_Data/georeference"
 
 img71 <- rast(paste0(imgDir, "/mosaic/m_07_16_71.tif"))
-dem <- rast("/media/hkropp/research/Kolyma_Data/dem/dem_mos.tif")
+dem <- rast("/media/hkropp/research/Kolyma_Data/dem/dem_mos_UTM.tif")
 smoke <- vect("/media/hkropp/research/Kolyma_Data/smoke_extent/smoke_extent.shp")
 extent <- vect("/media/hkropp/research/Kolyma_Data/m_extent/m_extent.shp")
-
+writeRaster()
 plot(img71, col=grey(1:100/100))
 plot(dem, col=grey(1:100/100))
 demProj <- project(dem, img71)
+writeRaster(demProj, "/media/hkropp/research/Kolyma_Data/dem/dem_mos_UTM.tif")
 
-smoke_mask <- img
+ext_rast <- project(extent, img71)
+ext_rast2 <- rasterize(ext_rast, img71)
+ext_mask <- mask(img71, ext_rast )
 
 # empty raster to resample
 # top coordinate
