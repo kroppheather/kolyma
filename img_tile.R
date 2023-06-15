8ef2cf0200dd21e680a32b8b884787cfa6ed9a84
kroppheather <kroppheather@gmail.com>
2023-06-15 13:59:24 -0400

    working on masks and setting up tiling

--- img_tile.R
+++ img_tile.R
@@ -9,10 +9,7 @@ library(dplyr)
 library(mapview)
 library(mapedit)
 library(stars)
-install.packages("leafpop")
-install.packages("mapview")
-install.packages("mapedit")
-install.packages("stars")
+
 
 imgDir <- "/media/hkropp/research/Kolyma_Data/georeference"
 
@@ -20,16 +17,19 @@ img71 <- rast(paste0(imgDir, "/mosaic/m_07_16_71.tif"))
 dem <- rast("/media/hkropp/research/Kolyma_Data/dem/dem_mos_UTM.tif")
 smoke <- vect("/media/hkropp/research/Kolyma_Data/smoke_extent/smoke_extent.shp")
 extent <- vect("/media/hkropp/research/Kolyma_Data/m_extent/m_extent.shp")
-writeRaster()
-plot(img71, col=grey(1:100/100))
-plot(dem, col=grey(1:100/100))
-demProj <- project(dem, img71)
-writeRaster(demProj, "/media/hkropp/research/Kolyma_Data/dem/dem_mos_UTM.tif")
+
 
 ext_rast <- project(extent, img71)
 ext_rast2 <- rasterize(ext_rast, img71)
-ext_mask <- mask(img71, ext_rast )
+ext_mask <- mask(img71, extent )
+img_mask <- mask(ext_mask, smoke, inverse=TRUE, filename=paste0(imgDir, "/mosaic/ext_mask_07_16_71.tif"))
+demExt <- ifel(dem > 100, 1, NA)
+img_mask2 <- mask(img_mask, demExt, inverse=TRUE, filename=paste0(imgDir, "/mosaic/lowland_07_16_71.tif"))
 
+plot(dem)
+plot(demExt)
+filename <- paste0(tempfile(), "_.tif")
+tileI <- makeTiles(img_mask, c(256,256), "/media/hkropp/research/Kolyma_Data/u_net71/tiles_256/img71.tif", na.rm=TRUE)
 # empty raster to resample
 # top coordinate
 
