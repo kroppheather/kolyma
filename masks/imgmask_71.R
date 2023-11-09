6c96634a6c9378e1e0474d81df7ab4a50b664206
kroppheather <kroppheather@gmail.com>
2023-11-09 14:08:57 -0500

    1971 training with extent exactly matched to 2020

--- masks/imgmask_71.R
+++ masks/imgmask_71.R
@@ -7,25 +7,28 @@
 library(terra)
 library(dplyr)
 
-dirD <- "/media/studentuser/STORE N GO/training"
+# first 50 masks were created by Adam
+
+dirD <- "/media/hkropp/research/Kolyma_Data/training/Kolyma/u_net71e/training"
 
 img <- list.files(paste0(dirD, "/img"),pattern=".tif")
 imgXML <- grepl(".aux.xml",img)
 img <- img[imgXML == FALSE ] 
 
-water <- list.files(paste0(dirD, "/masks/water"),pattern=".shp")
+water <- list.files(paste0(dirD, "/shapefiles/water"),pattern=".shp")
 waterXML <- grepl(".xml",water)
 water <- water[waterXML == FALSE ] 
+imgL <- list()
 
-shrubs <- list.files(paste0(dirD, "/masks/shrubs"),pattern=".shp")
+shrubs <- list.files(paste0(dirD, "/shapefiles/shrub"),pattern=".shp")
 shrubsXML <- grepl(".xml",shrubs)
 shrubs <- shrubs[shrubsXML == FALSE ] 
 
-trees <- list.files(paste0(dirD, "/masks/trees"),pattern=".shp")
+trees <- list.files(paste0(dirD, "/shapefiles/tree"),pattern=".shp")
 treesXML <- grepl(".xml",trees)
 trees <- trees[treesXML == FALSE ] 
 
-lowDensity <- list.files(paste0(dirD, "/masks/low_density"),pattern=".shp")
+lowDensity <- list.files(paste0(dirD, "/shapefiles/low"),pattern=".shp")
 lowDensityXML <- grepl(".xml",lowDensity)
 lowDensity <- lowDensity[lowDensityXML == FALSE ] 
 
@@ -45,28 +48,29 @@ for(i in 1:length(img)){
 
 }
 
-plot(imgL[[306]], col=grey(1:100/100))
+
 
 waterL <- list()
 for(i in 1:length(water)){
-  waterL[[i]] <- vect(paste0(dirD, "/masks/water/",water[i]))
+  waterL[[i]] <- vect(paste0(dirD, "/shapefiles/water/",water[i]))
 }  
 
 imgPos <- numeric()
 waterR <- list()
-imgPos <- which(imgNumber == waterNumber[10])
+imgPos <- which(imgNumber == waterNumber[1])
 for(i in 1:length(water)){
   imgPos <- which(imgNumber == waterNumber[i])
-  waterR[[i]] <- rast(waterL[[i]], imgL[[imgPos]], background=0)
+  waterR[[i]] <- rasterize(waterL[[i]], imgL[[imgPos]], background=0)
 }
 
+
 for(i in 1:length(water)){
-  writeRaster(waterR[[i]], paste0("/media/studentuser/STORE N GO/training/masks_img/water_img/", waterName[i] ))
+  writeRaster(waterR[[i]], paste0("/media/hkropp/research/Kolyma_Data/training/Kolyma/u_net71e/training/masks_img/water/", waterName[i] ))
 }
 
 shrubL <- list()
 for(i in 1:length(shrubs)){
-  shrubL[[i]] <- vect(paste0(dirD, "/masks/shrubs/",shrubs[i]))
+  shrubL[[i]] <- vect(paste0(dirD, "/shapefiles/shrub/",shrubs[i]))
 }  
 
 imgPos <- numeric()
@@ -74,20 +78,20 @@ shrubR <- list()
 imgPos <- which(imgNumber == shrubNumber[10])
 for(i in 1:length(shrubs)){
   imgPos <- which(imgNumber == shrubNumber[i])
-  shrubR[[i]] <- rast(shrubL[[i]], imgL[[imgPos]], background=0)
+  shrubR[[i]] <- rasterize(shrubL[[i]], imgL[[imgPos]], background=0)
 }
 
 plot(shrubL[[2]])
 plot(shrubR[[2]], add=TRUE, alpha=0.5)
 
 for(i in 1:length(shrubs)){
-  writeRaster(shrubR[[i]], paste0("/media/studentuser/STORE N GO/training/masks_img/shrubs_img/", shrubName[i] ))
+  writeRaster(shrubR[[i]], paste0("/media/hkropp/research/Kolyma_Data/training/Kolyma/u_net71e/training/masks_img/shrub/", shrubName[i] ))
 }
 
 
 treesL <- list()
 for(i in 1:length(trees)){
-  treesL[[i]] <- vect(paste0(dirD, "/masks/trees/",trees[i]))
+  treesL[[i]] <- vect(paste0(dirD, "/shapefiles/tree/",trees[i]))
 }  
 
 imgPos <- numeric()
@@ -95,21 +99,21 @@ treesR <- list()
 
 for(i in 1:length(trees)){
   imgPos <- which(imgNumber == treesNumber[i])
-  treesR[[i]] <- rast(treesL[[i]], imgL[[imgPos]], background=0)
+  treesR[[i]] <- rasterize(treesL[[i]], imgL[[imgPos]], background=0)
 }
 
 plot(treesL[[2]])
 plot(treesR[[2]], add=TRUE, alpha=0.5)
 
 for(i in 1:length(trees)){
-  writeRaster(treesR[[i]], paste0("/media/studentuser/STORE N GO/training/masks_img/trees_img/", treesName[i] ))
+  writeRaster(treesR[[i]], paste0("/media/hkropp/research/Kolyma_Data/training/Kolyma/u_net71e/training/masks_img/tree/", treesName[i] ))
 }
 
 
 
 lowDensityL <- list()
 for(i in 1:length(lowDensity)){
-  lowDensityL[[i]] <- vect(paste0(dirD, "/masks/low_density/",lowDensity[i]))
+  lowDensityL[[i]] <- vect(paste0(dirD, "/shapefiles/low/",lowDensity[i]))
 }  
 
 imgPos <- numeric()
@@ -117,11 +121,11 @@ lowDensityR <- list()
 
 for(i in 1:length(lowDensity)){
   imgPos <- which(imgNumber == lowDensityNumber[i])
-  lowDensityR[[i]] <- rast(lowDensityL[[i]], imgL[[imgPos]], background=0)
+  lowDensityR[[i]] <- rasterize(lowDensityL[[i]], imgL[[imgPos]], background=0)
 }
 
 for(i in 1:length(lowDensity)){
-  writeRaster(lowDensityR[[i]], paste0("/media/studentuser/STORE N GO/training/masks_img/low_density_img/", lowDensityName[i] ))
+  writeRaster(lowDensityR[[i]], paste0("/media/hkropp/research/Kolyma_Data/training/Kolyma/u_net71e/training/masks_img/low/", lowDensityName[i] ))
 }
 
 plot(l)
