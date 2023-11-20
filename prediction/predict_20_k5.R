6ed1f47fb7ee224a28cbb1e3733954ffca1644fd
kroppheather <kroppheather@gmail.com>
2023-11-20 15:55:37 -0500

    saving 2020 k5 map

--- prediction/predict_20_k5.R
+++ prediction/predict_20_k5.R
@@ -257,18 +257,18 @@ waterLayer <- rast("/media/hkropp/cold/k_5_2020/waterLayer.tif")
 # remove noise below set threshold
 
 # version 2 increases shrub threshold
-treeMap <- ifel(treeAll <= 0.7, 0, treeAll)
-waterMap <- ifel(waterAll <= 0.3, 0, waterAll)
-shrubMap <- ifel(shrubAll <= 0.3, 0, shrubAll)
-lowDMap <- ifel(lowDAll <= 0.1, 0, lowDAll)
+treeMap <- ifel(treeLayer <= 0.3, 0, treeLayer)
+waterMap <- ifel(waterLayer <= 0.5, 0, waterLayer)
+shrubMap <- ifel(shrubLayer <= 0.5, 0, shrubLayer)
+lowDMap <- ifel(lowDLayer <= 0.5, 0, lowDLayer)
 
 
 # binary map of above
 
-treeMapB <- ifel(treeAll <= 0.7, 0, 1)
-waterMapB <- ifel(waterAll <= 0.3, 0, 1)
-shrubMapB <- ifel(shrubAll <= 0.3, 0, 1)
-lowDMapB <- ifel(lowDAll <= 0.1, 0, 1)
+treeMapB <- ifel(treeLayer <= 0.3, 0, 1)
+waterMapB <- ifel(waterLayer <= 0.5, 0, 1)
+shrubMapB <- ifel(shrubLayer <= 0.5, 0, 1)
+lowDMapB <- ifel(lowDLayer <= 0.5, 0, 1)
 
 
 
@@ -318,4 +318,4 @@ plot(finalClass)
 
 
 
-writeRaster(finalClass, "/media/hkropp/research/Kolyma_Data/predictions/maps/class2020_k5_v2.tif", filetype="GTiff" )
+writeRaster(finalClass, "/media/hkropp/research/Kolyma_Data/predictions/maps/class2020_k5.tif", filetype="GTiff" )
