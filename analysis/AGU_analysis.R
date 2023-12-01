7fb7d5ba2938a27f88993541e6b6bdf41a7d8ef5
Heather Kropp <kroppheather@gmail.com>
2023-12-01 11:01:12 -0500

    Update AGU_analysis.R

--- analysis/AGU_analysis.R
+++ analysis/AGU_analysis.R
@@ -31,6 +31,7 @@ woody71 <- ifel(class71r == 1 | class71r == 3 | class71r == 4, 1, 0)
 shrub71 <- ifel(class71r == 3, 1, 0)
 taiga71 <- ifel(class71r == 1 | class71r == 4, 1, 0)
 plot(taiga71)
+plot(woody71)
 
 
 woody20 <- ifel(class20m == 1 | class20m == 3 | class20m == 4, 1, 0)
@@ -45,3 +46,37 @@ water71 <- ifel(class71r == 2, 1, 0)
 water20 <- ifel(class20m == 2, 1, 0)
 plot(water71)
 plot(water20)
+
+
+# look at change in cover
+changeF <- function(x, y){
+  ifelse(x == 0 & y == 0, 0, # no cover either year
+         ifelse(x == 1 & y == 0, 1, # loss of cover
+          ifelse(x == 0 & y == 1, 2, # gain of cover
+                 ifelse(x == 1 & y == 1, 3, NA)))) # stable cover
+}
+
+woodyC <- c(woody71, woody20)
+
+woodyChange <- lapp(woodyC, fun=changeF)
+plot(woodyChange)
+
+# look at water related changes
+waterC <- c(water71, water20)
+waterChange <- lapp(waterC, fun=changeF)
+plot(waterChange)
+
+# x = water change and y = woody 20
+hydroChange <- function(x, y){
+  ifelse(x == 1 & y == 2 , 1, # gain of woody in water loss
+       ifelse(x == 2 & y== 1,2, 0)) #loss of woody to water gain
+}
+
+hydroc <- c(waterChange, woodyChange)
+shrubHydro <- lapp(hydroc, hydroChange)
+# 1 = gain in woody cover due to water loss
+# 2 = loss of woody cover to gain of water
+plot(shrubHydro)
+
+# vectorize water
+
