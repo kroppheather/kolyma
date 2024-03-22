2629fc2cf98dd39d7edd94204d8a763397707ec9
Heather Kropp <kroppheather@gmail.com>
2024-03-22 15:49:20 -0400

    Update analysis.R

--- analysis/analysis.R
+++ analysis/analysis.R
@@ -19,4 +19,24 @@ valid20 <- st_read(paste0(dirData,"/valid/valid_class2020_2.shp"))
 plot(class71)
 plot(class20)
 
+# set the same boundary for both
 
+class71m <- mask(class71, bound)
+class20m <- mask(class20, bound)
+
+
+# resample
+class71r <- resample(class71m, class20m, method="near")
+plot(class71r)
+plot(class20m)
+
+
+#break into individual classes
+
+shrub71 <- ifel(class71r == 3, 1, 0)
+taiga71 <- ifel(class71r == 1 , 1, 0)
+water71 <- ifel(class71r == 2, 1, 0)
+
+shrub20 <- ifel(class20m == 3, 1, 0)
+taiga20 <- ifel(class20m == 1, 1, 0)
+water20 <- ifel(class20m == 2, 1, 0)
