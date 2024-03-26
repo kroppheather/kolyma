7d957f074f42740716e6d022ee86a570ccab5136
kroppheather <kroppheather@gmail.com>
2024-03-26 16:17:04 -0400

    version 2 of strat

--- prediction/predict_20_strat.R
+++ prediction/predict_20_strat.R
@@ -210,17 +210,17 @@ plot(treeLayer)
 # remove noise below set threshold
 
 # version 2
-treeMap <- ifel(treeLayer <= 0.3, 0, treeLayer)
+treeMap <- ifel(treeLayer <= 0.5, 0, treeLayer)
 waterMap <- ifel(waterLayer <= 0.8, 0, waterLayer)
-shrubMap <- ifel(shrubLayer <= 0.6, 0, shrubLayer)
+shrubMap <- ifel(shrubLayer <= 0.5, 0, shrubLayer)
 
 
 
 # binary map of above
 
-treeMapB <- ifel(treeLayer <= 0.3, 0, 1)
+treeMapB <- ifel(treeLayer <= 0.5, 0, 1)
 waterMapB <- ifel(waterLayer <= 0.8, 0, 1)
-shrubMapB <- ifel(shrubLayer <= 0.6, 0, 1)
+shrubMapB <- ifel(shrubLayer <= 0.5, 0, 1)
 
 
 
@@ -269,4 +269,4 @@ plot(finalClass)
 
 
 
-writeRaster(finalClass, "/media/hkropp/research/Kolyma_Data/predictions/v2/maps/class2020_strat_v1.tif", filetype="GTiff" )
+writeRaster(finalClass, "/media/hkropp/research/Kolyma_Data/predictions/v2/maps/class2020_strat_v2.tif", filetype="GTiff" )
